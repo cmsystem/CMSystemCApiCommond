3920a54c859292ca95a500d23845ce5b1c503409
camilo <acmcamilo@gmail.com>
2026-03-28 12:36:37 -0500

    cambio para testing

--- src/main/java/com/pe/cmsystem/api/commond/service/CMSystemKeyServiceImpl.java
+++ src/main/java/com/pe/cmsystem/api/commond/service/CMSystemKeyServiceImpl.java
@@ -1,6 +1,7 @@
 package com.pe.cmsystem.api.commond.service;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -30,6 +31,22 @@ public class CMSystemKeyServiceImpl implements CMSystemKeyService {
 
     @Value("${crypto.pbkdf2.tipe-algorithm}")
     private String tipealgorithm;
+
+    @Autowired
+    public CMSystemKeyServiceImpl(
+            @Value("${crypto.pbkdf2.iterations}") int iterations,
+            @Value("${crypto.pbkdf2.key-length}") int keyLength,
+            @Value("${crypto.pbkdf2.algorithm}") String algorithm,
+            @Value("${crypto.pbkdf2.secretKey}") String secretKey,
+            @Value("${crypto.pbkdf2.saltBase64}") String saltBase64,
+            @Value("${crypto.pbkdf2.tipe-algorithm}") String tipealgorithm) {
+        this.iterations = iterations;
+        this.keyLength = keyLength;
+        this.algorithm = algorithm;
+        this.secretKey = secretKey;
+        this.saltBase64 = saltBase64;
+        this.tipealgorithm = tipealgorithm;
+    }
 
     public SecretKey deriveKeyFromClient() throws Exception {
         byte[] salt = Base64.getDecoder().decode(saltBase64);
